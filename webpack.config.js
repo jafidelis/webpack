773bf5689f2ddcf2aa1a0be6b404a02931251f25
José Alexandre <jafidelis@gmail.com>
2018-07-26 22:09:15 -0300

    adicionado preloader sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
+const modoDev = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'estilo.css'
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin()
+        ]
+    },
     module: {
         rules: [{
-            test: /\.css$/,
+            test: /\.s?[ac]ss$/,
             use: [
-                'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader' // interpreta @import, url()...
+                { loader: MiniCssExtractPlugin.loader },
+
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
+                'sass-loader'
             ]
         }]
     }
